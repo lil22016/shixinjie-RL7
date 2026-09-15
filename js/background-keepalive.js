c9692bb3e191474b4bd6680a7bd948b199026af5
lkkzy <165129636+lil22016@users.noreply.github.com>
2026-09-15 19:58:00 -0400

    Add files via upload

--- js/background-keepalive.js
+++ js/background-keepalive.js
@@ -1,4 +1,4 @@
-/* RL7 Background Keepalive v23
+/* RL7 Background Keepalive v24
  * Best-effort iOS media keepalive. Designed to create a real MediaSession so
  * iOS can expose it on Lock Screen / Control Center / Dynamic Island when allowed.
  */
